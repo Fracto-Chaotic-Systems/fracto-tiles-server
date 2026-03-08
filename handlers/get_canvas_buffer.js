4f4c04302d773c518bc82937c0e4a25941fb7cad
media-frameworks <mikehallvideo@gmail.com>
2026-03-07 22:49:24 -0500

    get_canvas_buffer

---
+++ handlers/get_canvas_buffer.js
@@ -0,0 +1,27 @@
+import {
+   fill_canvas_buffer,
+   init_canvas_buffer
+} from "../../../sdk/FractoTileData.js";
+
+export const handle_get_canvas_buffer = async (req, res) => {
+   const width_px = parseInt(req.query.width_px)
+   const scope = parseFloat(req.query.scope)
+   const focal_point = {
+      x: parseFloat(req.query.focal_point_x),
+      y: parseFloat(req.query.focal_point_y),
+   }
+   const aspect_ratio = parseFloat(req.query.aspect_ratio)
+   const resolution_factor = parseFloat(req.query.resolution_factor)
+   const canvas_buffer = init_canvas_buffer(width_px, aspect_ratio);
+   // console.log('handle_get_canvas_buffer 1')
+   await fill_canvas_buffer(
+      canvas_buffer,
+      width_px,
+      focal_point,
+      scope,
+      aspect_ratio,
+      resolution_factor,
+   )
+   // console.log('handle_get_canvas_buffer 2')
+   res.json({canvas_buffer})
+}
--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import {handle_main_status} from "./handlers/status.js";
 import {handle_tile} from "./handlers/tile.js";
 import {initialize_coverage} from "../../sdk/FractoCoverageUtils.js";
 import {handle_logs} from "./handlers/logs.js";
+import FractoIndexedTiles from "../../sdk/FractoIndexedTiles.js";
+import {get_manifest} from "../../sdk/FractoTileData.js";
+import {handle_get_canvas_buffer} from "./handlers/get_canvas_buffer.js";
+import {handle_tile_coverage} from "./handlers/tile_coverage.js";
 
 const app = express();
 
@@ -16,13 +20,22 @@ app.use((req, res, next) => {
    next();
 });
 
-// Start the server and listen for incoming requests
-app.listen(FRACTO_TILES_PORT, () => {
-   console.log(chalk.green(`fracto-tiles-server is running on http://localhost:${FRACTO_TILES_PORT}`));
-   initialize_coverage()
-});
+FractoIndexedTiles.init_tile_sets()
 
+get_manifest((file) => {
+   console.log(file.manifest_file);
+}, () => {
+   initialize_coverage(() => {
+      // Start the server and listen for incoming requests
+      app.listen(FRACTO_TILES_PORT, () => {
+         console.log(chalk.green(`fracto-tiles-server is running on http://localhost:${FRACTO_TILES_PORT}`));
+      });
+   })
+})
 
 app.get('/', handle_main_status)
 app.get('/tile', handle_tile)
 app.get('/logs', handle_logs)
+
+app.get("/tile_coverage", handle_tile_coverage)
+app.get("/canvas_buffer", handle_get_canvas_buffer)
