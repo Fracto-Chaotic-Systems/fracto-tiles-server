aa4b0048162703fb3e713c7ee3f74d75ed063060
media-frameworks <mikehallvideo@gmail.com>
2026-05-17 17:57:51 -0400

    added totals

--- handlers/tile_inventory.js
+++ handlers/tile_inventory.js
@@ -7,15 +7,23 @@ const processJsonFiles = async (directoryPath) => {
    try {
       const files = fs.readdirSync(directoryPath);
       const jsonFiles = files.filter(file => path.extname(file).toLowerCase() === '.json');
+      let grand_total = 0
+      const totals = {}
       for (const file of jsonFiles) {
          const filePath = path.join(directoryPath, file);
          const fileData = fs.readFileSync(filePath, 'utf-8');
          const jsonData = JSON.parse(fileData);
          const tile_paths = Object.keys(jsonData)
+         let total = 0;
          for (const tile_path of tile_paths) {
-            console.log(`tile_path: ${tile_path}`);
+            total += jsonData[tile_path].length
+            grand_total += jsonData[tile_path].length
+            console.log(`tile_path: ${tile_path}`, jsonData[tile_path].length);
          }
+         totals[file] = total
       }
+      console.log(totals);
+      console.log('grand total', grand_total);
    } catch (error) {
       console.error('Error processing folder:', error);
    }
